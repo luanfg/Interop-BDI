888f33a9e1ef2353681414d14cc18d572b7f0879
UFRGS <UFRGS@143.54.13.148>
2016-09-08 16:27:34 -0300

    Adicionada implementação do algoritmo de médias móveis

---
+++ src/interop/movingMeans/MovingMeans.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interop.movingMeans;
+
+import interop.log.model.LogValuePair;
+import interop.log.util.LogValueList;
+
+/**
+ *
+ * @author UFRGS
+ */
+public class MovingMeans {
+    
+    public LogValueList apply(LogValueList logList, int windowSize){
+        LogValueList result = new LogValueList();
+        float parSum = 0;
+        int inWindow;
+        for(inWindow = 0; inWindow < windowSize/2; inWindow++){
+            parSum += logList.get(inWindow).getLogValue();
+        }
+        for(int i = 0; i < logList.size();i++){
+            if(i+windowSize/2 < logList.size()){
+                 parSum += logList.get(i+windowSize/2).getLogValue();
+                 inWindow++;
+            }
+            if(i-1-windowSize/2 > 0){
+                parSum -= logList.get(i-1-windowSize/2).getLogValue();
+                inWindow--;
+            };
+            LogValuePair pair = new LogValuePair(logList.get(i).getDepth(), parSum/inWindow);
+            result.add(pair);
+        }
+        return result;
+    }
+}
