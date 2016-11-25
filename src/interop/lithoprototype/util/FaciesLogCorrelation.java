134e69a362f35b0aa4735fb2c9ad939504d8ec3e
BRUNOZ <BRUNOZ@BRUNOZ-PC>
2016-11-25 16:12:50 -0200

    Now this class actually do something

--- src/interop/lithoprototype/util/FaciesLogCorrelation.java
+++ src/interop/lithoprototype/util/FaciesLogCorrelation.java
@@ -6,8 +6,8 @@
 package interop.lithoprototype.util;
 
 import interop.stratigraphic.model.DepositionalFacies;
-import interop.log.model.WellLog;
-import interop.log.util.LogValueList;
+import interop.log.model.*;
+import interop.log.util.*;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -21,12 +21,26 @@ public class FaciesLogCorrelation {
     DepositionalFacies facies;
     List<WellLog> wellLogsSlice;
     
-    public void FaciesLogCorrelation(DepositionalFacies depositionalFacies, List<WellLog> wellLogs)
+    /*
+    
+    */
+    public FaciesLogCorrelation(DepositionalFacies depositionalFacies, ParsedLAS parsed)
     {
         setDepositionalFacies(depositionalFacies);
+        wellLogsSlice = this.sliceWellLog(parsed);
+    }
+    
+    public static List<FaciesLogCorrelation> logAndDescriptionsToListOfFaciesLogCorrelations(List<DepositionalFacies> depositionalFacies, ParsedLAS parsed)
+    {
+        List<FaciesLogCorrelation> listOfFaciesLogCorrelation = new ArrayList<>();
         
+        for(DepositionalFacies df:depositionalFacies)
+        {
+            FaciesLogCorrelation flc = new FaciesLogCorrelation(df, parsed);
+            listOfFaciesLogCorrelation.add(flc);
+        }
         
-        
+        return listOfFaciesLogCorrelation;
     }
     
     /**
@@ -54,7 +68,7 @@ public class FaciesLogCorrelation {
      */
     public int getLithologyUID()
     {
-        return Integer.parseInt( facies.getLithology().getValue() );
+        return facies.getLithology().getId();
     }
     
     public float getBottomMeasure()
@@ -67,45 +81,97 @@ public class FaciesLogCorrelation {
         return facies.getTopMeasure();
     }
     
+    public int getNumberOfSamples()
+    {
+        WellLog anyWellLog = wellLogsSlice.get(0);
+        LogValueList anyLogValueList = anyWellLog.getLogValues();
+        return anyLogValueList.size();
+    }
+    
     /**
      *  Slice the list of WellLog maintaining the data of the depths that corresponds to the specified depositional facies. 
      * @param wellLogs the complete wellLog received as parameter at the constructor.
      * @return the list of WellLog containing just the data of the depths that corresponds to the specified depositional facies.
      */
-    private List<WellLog> sliceWellLog(List<WellLog> wellLogs)
+    private List<WellLog> sliceWellLog(ParsedLAS parsed)
     {
-        List<WellLog> slicedWellLogs = new ArrayList();
-     
-        for (WellLog wellLog : wellLogs)
+        //System.out.println("Number of log samples: " + wellLogs.get(0).getLogValues().size());
+        List<WellLog> slicedWellLogs = new ArrayList<>();
+        //System.out.println("**In sliceWellLog of FaciesLogCorrelation**\n Well Logs Size: " + wellLogs.size() + "\n***********\n");
+        
+        
+        float faciesTopMeasure = facies.getTopMeasure();
+        float faciesBottomMeasure = facies.getBottomMeasure();
+        float logTopMeasure = parsed.getStartDepth();
+        float logBottomMeasure = parsed.getStopDepth();
+        float logStep = parsed.getStepValue();
+        int startIndex=0;
+        int stopIndex=0;
+        
+        //System.out.println("faciesTopMeasure: " + faciesTopMeasure + "\nfaciesBottomMeasure: " + faciesBottomMeasure + "\nlogTopMeasure: " + logTopMeasure + "\nlogBottomMeasure: " + logBottomMeasure + "\nlogStep: " + logStep);
+        if(faciesTopMeasure >= logTopMeasure)
         {
-            WellLog slicedWellLog = new WellLog();
+            float diffTop = faciesTopMeasure - logTopMeasure;
+            int diffTopSteps = (int)(diffTop/logStep);
+       
+            /*
+            System.out.println("diffTop: " + diffTop + "\ndiffTopSteps: " + diffTopSteps);
+            System.out.println((diffTopSteps-(int)diffTopSteps));
+            System.out.println("Calculated Log Top Measure: " + parsed.getLogsList().get(0).getLogValues().get(diffTopSteps).getDepth());
+            System.out.println();
+            */
+            startIndex = diffTopSteps;
             
-            slicedWellLog.setLogType( wellLog.getLogType() );
-            slicedWellLog.setNullValue( wellLog.getNullValue() );
-            
-            LogValueList originalLogValueList = wellLog.getLogValues();
-            int i = 0, listSize = wellLogs.size();
-            float currentDepth = originalLogValueList.get(i).getDepth(); // get initial depth
-            while(currentDepth < facies.getTopMeasure())
+            if(faciesBottomMeasure > logBottomMeasure)
             {
-                currentDepth = originalLogValueList.get(i).getDepth();
-                i++;
-            }
-            
-            
-            LogValueList slicedLogValueList = new LogValueList();
-            while( (currentDepth >= facies.getBottomMeasure()) && (i < listSize) )
-            {
-                currentDepth = originalLogValueList.get(i).getDepth();
+                float diff = faciesBottomMeasure - faciesTopMeasure;
+                int diffBottomSteps = diffTopSteps + (int)(diff/logStep);
                 
-                slicedLogValueList.add( originalLogValueList.get(i) );
-            
-                i++;
+                stopIndex = diffBottomSteps;
             }
-            
-            slicedWellLog.setLogValues(slicedLogValueList);
+            else
+            {
+                stopIndex = parsed.getLogsList().get(0).getLogValues().size()-1;
+            }
         }
         
+        List<WellLog> wellLogs = parsed.getLogsList();
+        
+        for(WellLog wl:wellLogs)
+        {
+            WellLog slicedWellLog = new WellLog();
+            slicedWellLog.setLogType(wl.getLogType());
+            slicedWellLog.setLogMeasureUnit(wl.getLogMeasureUnit());
+            slicedWellLog.setLogDescription(wl.getLogDescription());
+            slicedWellLog.setNullValue(wl.getNullValue());
+            
+            LogValueList slicedLVL = new LogValueList();
+            slicedLVL.addAll( wl.getLogValues().subList(startIndex, stopIndex)      );       
+          
+            slicedWellLog.setLogValues(slicedLVL);
+            slicedWellLogs.add(slicedWellLog);
+        }
+     
         return slicedWellLogs;
+    }
+    
+    public void print()
+    {
+        System.out.println("***Facies Log Correlation***");
+        
+        System.out.println("Top Measure: " + this.getTopMeasure());
+        System.out.println("Bottom Measure: " + this.getBottomMeasure());
+        System.out.println("Lithology UID; " + this.getLithologyUID());
+        System.out.println("Number of Samples: " + this.getNumberOfSamples());
+        
+        /*
+        System.out.println("SAMPLES: \nDEPTH\t" + wellLogsSlice.get(0).getLogType());
+        for(LogValuePair lvp:wellLogsSlice.get(0).getLogValues())
+        {
+            System.out.println(lvp.getDepth() + "\t" + lvp.getLogValue());
+        }
+        */
+        
+        System.out.println("****************************\n");
     }
 }
