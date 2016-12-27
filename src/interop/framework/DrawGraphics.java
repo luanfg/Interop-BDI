a9c4f6307921840b655faf18ef3aa220addd7a80
eduardo <eduardo@10.0.2.15>
2016-12-27 09:38:46 -0200

    refactoring DrawGraphics

--- src/interop/framework/DrawGraphics.java
+++ src/interop/framework/DrawGraphics.java
@@ -11,7 +11,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-//import interop.movingMeans.MovingMeans;
+import interop.movingMeans.MovingMeans;
 
 /**
  *
@@ -28,39 +28,33 @@ public class DrawGraphics extends JPanel{
         setBackground(Color.lightGray);
     }
     
+    void writesTheLogMeasureUnits(int i, Graphics g){
+            double whereToPutTheLogName = i * width / (parsed.getLogsList().size()+2) ;
+            String nome = parsed.getLogsList().get(i).getLogMeasureUnit();
+            g.drawString(nome, (int)whereToPutTheLogName, 12);
+    }
+    
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
              
         for( int i =0; i < parsed.getLogsList().size() ; i++ ){//for each log....
-            
-            double whereToPutTheLogName = i * width / (parsed.getLogsList().size()+2) ;
-            
-            String nome = parsed.getLogsList().get(i).getLogMeasureUnit();
-            
-            g.drawString(nome, (int)whereToPutTheLogName, 12);
-            
-            boolean dontHaveFirstBit=true; 
+   
+            boolean dontHaveFirstBit=true;//for the first value only 
             double wellSize = parsed.getStopDepth() - parsed.getStartDepth();
             int x_ant=-1, y_ant=-1;
             
-            //find out the max and min values of each log 
-            //PS: this should be in the ParsedLAS class
-            double maxValue=-99999; //LAS file can have values bigger than 99999?
-            double minValue=99999;
-            for(int p=0; p<(int)wellSize*5; p++ ){//for the beginning to an end of a log
-                double q =parsed.getLogsList().get(i).getLogValues().get(p).getLogValue();
-                if(q>maxValue && q!=parsed.getNullValue()){
-                     maxValue=q;
-                }
-                if(q<minValue && q!=parsed.getNullValue()){
-                    minValue=q;
-                }
-            }
-                               
+            writesTheLogMeasureUnits(i, g);
+            
+            double maxValue = parsed.getMaxValue(i);
+            double minValue = parsed.getMinValue(i);
+            
+            //Applying MovingMeans function
+            LogValueList list = MovingMeans.apply(parsed.getLogsList().get(i).getLogValues(), 1);
+            
             for( int j=0; j<(int)wellSize*5; j++ ){//for the beginning to an end of a log // *5 pois cada poço é avaliado a cada 0.2km, wellSize é em km
-                double x=parsed.getLogsList().get(i).getLogValues().get(j).getLogValue();
-                double y=parsed.getLogsList().get(i).getLogValues().get(j).getDepth();
+                double x = list.get(j).getLogValue();
+                double y = list.get(j).getDepth();
                 
                 if(x != parsed.getNullValue()){                       
                        double screenx = (x-minValue) * width / (maxValue-minValue) / (parsed.getLogsList().size()+2) + i* width / (parsed.getLogsList().size()+2) ;
