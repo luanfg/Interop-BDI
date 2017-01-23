2ff5e17bf6c57084ee7cee6419ea517ef85a71dc
Eduardo <Eduardo@Eduardo-PC>
2017-01-23 15:35:32 -0200

    added method to get the RGB code

--- src/interop/stratigraphic/model/StratigraphicRockColor.java
+++ src/interop/stratigraphic/model/StratigraphicRockColor.java
@@ -13,11 +13,34 @@ import interop.stratigraphic.control.SQLite;
  */
 public class StratigraphicRockColor extends AbstractStratigraphicAttribute
 {
+    
+    protected String rgbCode = null;
+    
 
     @Override
     protected void updateValue() {
         SQLite sql = new SQLite();
         this.value = sql.readValue(AttributeType.RockColor, this.getId());
     }
+
+    /**
+     * Returns the RGB code of that specific attribute instance as a String. This value is filled accessing the database.
+     * @return 
+     */
+    public String getRGBCode()
+    {
+        if(rgbCode == null)
+            updateRGBCode();
+        
+        return rgbCode;
+    }
+    
+    protected void updateRGBCode(){
+        SQLite sql = new SQLite();
+        this.rgbCode = sql.readValue(AttributeType.RockColor, "VALUE_HEX", this.getId());
+    }
+    
+    
+    
     
 }
