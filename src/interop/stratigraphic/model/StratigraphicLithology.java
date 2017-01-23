a049d47baeba3611052b7bfb490b477814fadabb
Eduardo <Eduardo@Eduardo-PC>
2017-01-23 15:36:09 -0200

    added method to get the SVG texture

--- src/interop/stratigraphic/model/StratigraphicLithology.java
+++ src/interop/stratigraphic/model/StratigraphicLithology.java
@@ -13,11 +13,30 @@ import interop.stratigraphic.control.SQLite;
  */
 public class StratigraphicLithology extends AbstractStratigraphicAttribute
 {
-
+    protected String textureSVG = null;
+    
     @Override
     protected void updateValue() {
         SQLite sql = new SQLite();
         this.value = sql.readValue(AttributeType.Lithology, this.getId());
+    }
+
+    /**
+     * Returns the textude on svg format of that specific attribute instance as a String. This value is filled accessing the database.
+     * @return 
+     */
+    public String getTextureSVG()
+    {
+        if(textureSVG == null)
+            updateTextureSVG();
+        
+        return textureSVG;
+    }
+    
+    protected void updateTextureSVG(){
+        SQLite sql = new SQLite();
+        this.textureSVG = sql.readValue(AttributeType.Lithology, "VALUE_PATTERN_SVG", this.getId());//or is it VALUE_PATTERN?
+        
     }
     
 }
